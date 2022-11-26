1ca1887386322f2ae7dad0ed5e515a661e4bef8a
Kiriraksmey <raksmeyvn98@gmail.com>
2022-11-26 11:54:34 +0700

    updateview

---
+++ src/main/resources/static/front-end/assets/js/collape.js
@@ -0,0 +1,17 @@
+
+    jQuery(function ($) {
+    $('.panel-heading span.clickable').on("click", function (e) {
+        if ($(this).hasClass('panel-collapsed')) {
+            // expand the panel
+            $(this).parents('.panel').find('.panel-body').slideDown();
+            $(this).removeClass('panel-collapsed');
+            $(this).find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+        }
+        else {
+            // collapse the panel
+            $(this).parents('.panel').find('.panel-body').slideUp();
+            $(this).addClass('panel-collapsed');
+            $(this).find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+        }
+    });
+});
