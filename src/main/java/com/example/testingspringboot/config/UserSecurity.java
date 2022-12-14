a905ea10ddaff729621723a57d7676e19db5f54f
Kiriraksmey <raksmeyvn98@gmail.com>
2022-12-14 15:42:34 +0700

    update

---
+++ src/main/java/com/example/testingspringboot/config/UserSecurity.java
@@ -0,0 +1,23 @@
+//package com.example.testingspringboot.config;
+//
+//import com.example.testingspringboot.Repository.UserRepository;
+//import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component("userSecurity")
+//public class UserSecurity {
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    public boolean hasUserId(Authentication authentication, Long id) {
+//
+//        long userID= userRepository.findByUserName(authentication.name()).getId();
+////		System.out.println(userId+"  "+userID);
+//        if(userID==id)
+//            return true;
+//
+//        return false;
+//
+//    }
+//}
